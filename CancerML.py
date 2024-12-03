c8ab363897b2a39432b339674fa3986687638f81
Oguz-Kaan Öztürk <Kaanzk@outlook.de>
2024-12-03 23:43:01 +0100

    implemented model

--- CancerML.py
+++ CancerML.py
@@ -13,15 +13,15 @@ train_dataset = dataset.sample(frac=0.8, random_state=0)
 test_dataset = dataset.drop(train_dataset.index)
 #train_dataset.describe().transpose()
 
-train_features = train_dataset.copy()
+cancer_features = train_dataset.copy()
 test_features = test_dataset.copy()
 
-train_labels = train_features.pop('diagnosis')
+cancer_labels = cancer_features.pop('diagnosis')
 test_labels = test_features.pop('diagnosis')
 
 inputs = {}
 
-for name, column in train_features.items():
+for name, column in cancer_features.items():
     dtype = column.dtype
     if dtype == object:
         dtype = tf.string
@@ -43,7 +43,7 @@ preprocessed_inputs = [all_numeric_inputs]
 for name, input in inputs.items():
     if input.dtype == tf.float32:
         continue
-    lookup = layers.StringLookup(vocabulary=np.unique(train_features[name]))
+    lookup = layers.StringLookup(vocabulary=np.unique(cancer_features[name]))
     one_hot = layers.CategoryEncoding(num_tokens=lookup.vocabulary_size())
 
     x = lookup(input)
@@ -52,4 +52,22 @@ for name, input in inputs.items():
 
 preprocessed_inputs_cat = layers.Concatenate()(preprocessed_inputs)
 cancer_preprocessing = tf.keras.Model(inputs, preprocessed_inputs_cat)
-tf.keras.utils.plot_model(model = cancer_preprocessing, rankdir="LR", dpi=72, show_shapes=True)
+#tf.keras.utils.plot_model(model = cancer_preprocessing, rankdir="LR", dpi=72, show_shapes=True)
+
+cancer_features_dict = {name: np.array(value) for name, value in cancer_features.items()}
+features_dict = {name:values[:1] for name, values in cancer_features_dict.items()}
+print(cancer_preprocessing(features_dict))
+
+def cancer_model(preprocessing_head, inputs):
+    body = tf.keras.Sequential([
+        layers.Dense(64, activation='relu'),
+        layers.Dense(1)
+    ])
+    preprocessed_inputs = preprocessing_head(inputs)
+    result = body(preprocessed_inputs)
+    model = tf.keras.Model(inputs, result)
+
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),optimizer=tf.keras.optimzers.Adam())
+    return model
+
+cancer_model.fit(x=cancer_features_dict, y=cancer_labels, epochs=10)    
